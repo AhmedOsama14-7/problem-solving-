d67220ce44b0e7ec1c9014511d9e87e2af8b38b4
AhmedOsama14-7 <hamodaoss209@gmail.com>
2024-08-11 19:48:18 +0300

    end

---
+++ index.js
@@ -0,0 +1,117 @@
+// problem 1
+
+// var num= +prompt("enter number");
+
+// if (num <= 100) {
+//     console.log("Less than or equal 100")
+// } else {
+//     console.log("Number greater than 100")
+// }
+
+
+
+//  problem 2
+
+
+//  var txt = prompt("enter text");
+// if (txt.length > 100){
+//     console.log("string is long")
+// } else {
+//     console.log("string is short")
+// }
+
+//  problem 3
+
+// var password = prompt("Enter pasword")
+
+// if (password == "confirm password"){
+//     console.log("password valid")
+// } else {
+//     console.log("no vaild")
+// }
+
+
+//  problem 4
+
+// var day = prompt("Enter the day")
+
+// if(day == "saterday"){
+//     console.log("It is weekend")
+// } else{
+//     console.log("work day")
+// }
+
+
+// problem 5
+
+// var num = +prompt("enter number");
+
+// if(num > 0) {
+//     console.log("positive")
+// } else if (num < 0 ) {
+//     console.log("negative")
+// } else {
+//     console.log("zero")
+// }
+
+// problem 6
+
+// var deg = +prompt("enter degree");
+
+// if (deg < 50){
+//     console.log("F")
+// } else if( deg >= 50 && deg <= 69  ) {
+//     console.log("D");
+    
+// } else if( deg >= 70 && deg <= 79 ){
+//     console.log("C");
+
+// } else if( deg >= 80 && deg <= 89 ){
+//     console.log("B");
+
+// }  else {
+//     console.log("A")
+// }
+
+// problem 7
+
+// var temp = +prompt("enter tempreture degree")
+
+// if ( temp > 30){
+//     console.log("Hot")
+// } else {
+//     console.log("cold")
+// }
+
+
+// problem 8
+
+// var num = +prompt("enter number")
+
+// if ( num % 2 == 0) {
+//     console.log("even")
+// } else{
+//     console.log("odd")
+// }
+
+
+//  problem 9
+
+// var num = +prompt("enter num");
+
+// if ( num % 2 == 0){
+//     console.log("even");
+
+//     if( num > 20){
+//         console.log("enter university")
+//     } else {
+//         console.log("dont enter university")
+//     }
+// } else {
+//     console.log("odd")
+//     if( num > 20){
+//         console.log("enter university")
+//     } else {
+//         console.log("dont enter university")
+//     }
+// }
